ba42dca619bc8083c56797c452fa507b26602217
cris21david <39814606+cris21david@users.noreply.github.com>
2018-09-13 18:22:21 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,45 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		},
+		{
+		path: '/info-producto/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		},
+		{
+		path: '/categoria/',
+    	url: 'categoria.html',
+    	name: 'categoria',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
